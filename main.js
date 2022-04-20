b8ee63f801c95fff175da82930eeb075ddd56e91
nchz <nchngz@gmail.com>
2022-04-20 12:42:17 -0300

    add global and image timers.

--- main.js
+++ main.js
@@ -1,15 +1,71 @@
-var timerId;
-function buildTimer() {
-  const seconds = Number(document.getElementById("timeoutSeconds").value);
-  return setInterval(endGame, 1000 * seconds);
+var globalTimer;
+var imageTimer;
+var wordTimer;
+function deleteTimers() {
+  clearInterval(globalTimer);
+  clearInterval(imageTimer);
+  clearInterval(wordTimer);
+  globalTimer = undefined;
+  imageTimer = undefined;
+  wordTimer = undefined;
+}
+
+// TODO improve this.
+function buildTimer(func, elemIdOrSecs) {
+  var seconds;
+  if (typeof (elemIdOrSecs) == "string") {
+    // get seconds from ElementId.
+    seconds = Number(document.getElementById(elemIdOrSecs).value);
+  } else {
+    seconds = elemIdOrSecs;
+  }
+  return setInterval(func, 1000 * seconds);
+}
+
+function getText() {
+  return document.getElementById("storyText");
+}
+
+function waitFor(vars, callback) {
+  var timerId = setInterval(function () {
+    const allReady = vars.map(v => window[v]).every(t => t);
+    if (allReady) {
+      clearInterval(timerId);
+      console.log(`Variables ready: ${vars}`);
+      callback();
+    }
+  }, 100);
+}
+
+waitFor(["randomImageList", "randomWordList"], () => { });
+getText().addEventListener("keydown", startGame);
+
+// TODO pressing ANY key, like ClientRectList, starts the Gamepad.
+function startGame() {
+  const storyText = getText();
+  storyText.removeEventListener("keydown", startGame);
+  storyText.addEventListener("keydown", processWord);
+
+  document.getElementById("targetWord").innerHTML = getRandomWord();
+  imageTimer = buildTimer(rotateRandomImage, 10);
+
+  globalTimer = buildTimer(endGame, "globalTimeoutSecs");
 }
 
 function endGame() {
-  clearInterval(timerId);
-  const storyText = document.getElementById("storyText");
+  deleteTimers();
+  makeTextReadonly();
+  alertWordCount();
+}
+
+function makeTextReadonly() {
+  const storyText = getText();
   storyText.setAttribute("readonly", "true");
   storyText.setAttribute("style", "background: transparent;");
-  // count words.
+}
+
+function alertWordCount() {
+  const storyText = getText();
   const inputWords = storyText.value.trim().split(/\s+/);
   const msg = `
     Se acabo el tiempo!
@@ -20,31 +76,43 @@ function endGame() {
   alert(msg);
 }
 
-function resetTimer() {
-  clearInterval(timerId);
-  timerId = buildTimer();
-}
-
-document.getElementById("storyText").addEventListener("keydown", processWord);
 function processWord(e) {
+  // when space or enter is PermissionRequestedEvent.
   if (e.keyCode == 13 || e.keyCode == 32) {
-    resetTimer();
+    resetWordTimer();
     checkTargetWord();
   }
 }
+
+function resetWordTimer() {
+  clearInterval(wordTimer);
+  wordTimer = buildTimer(endGame, "timeoutSeconds");
+}
+
 function checkTargetWord(e) {
   // check if target word was used.
-  const inputWords = document.getElementById("storyText").value.trim().split(/\s+/);
+  const inputWords = getText().value.trim().split(/\s+/);
   const lastWord = inputWords[inputWords.length - 1];
   const targetWord = document.getElementById("targetWord").innerHTML;
-  if (targetWord === ". . .") {
-    document.getElementById("targetWord").innerHTML = getRandomWord();
-  } else if (normString(lastWord) === normString(targetWord)) {
-    document.getElementById("targetWord").innerHTML = getRandomWord();
+  if (normString(lastWord) === normString(targetWord)) {
     const prevWords = document.getElementById("prevWords")
     prevWords.innerHTML = "<div>" + targetWord + "</div>" + prevWords.innerHTML;
-    document.getElementById("randomImage").setAttribute("src", getRandomImage());
+    // rotateRandomWord is not working.
+    document.getElementById("targetWord").innerHTML = getRandomWord();
   }
+}
+
+function resetText() {
+  const storyText = getText();
+  storyText.value = "";
+  storyText.removeAttribute("readonly");
+  storyText.removeAttribute("style");
+}
+
+function resetRandomWords() {
+  document.getElementById("targetWord").innerHTML = "";
+  document.getElementById("prevWords").innerHTML = "";
+  document.getElementById("randomImage").removeAttribute("src");
 }
 
 var randomImageList;
@@ -54,6 +122,9 @@ function getRandomImage() {
   const i = Math.floor(Math.random() * randomImageList?.length);
   return randomImageList?.slice(i, i + 1) || "";
 }
+function rotateRandomImage() {
+  document.getElementById("randomImage").setAttribute("src", getRandomImage());
+}
 
 var randomWordList;
 const urlWords = "https://raw.githubusercontent.com/mazyvan/most-common-spanish-words/master/most-common-spanish-words-v4.txt";
@@ -62,23 +133,19 @@ function getRandomWord() {
   const i = Math.floor(Math.random() * randomWordList?.length);
   return randomWordList?.slice(i, i + 1) || ". . .";
 }
-document.getElementById("targetWord").innerHTML = getRandomWord();
+// function rotateRandomWord() {
+//   document.getElementById("targetWord").innerHtml = getRandomWord();
+// }
 
 function resetGame() {
-  clearInterval(timerId);
-  // reset text.
-  const storyText = document.getElementById("storyText");
-  storyText.value = "";
-  storyText.removeAttribute("readonly");
-  storyText.removeAttribute("style");
-  // reset random words.
-  document.getElementById("targetWord").innerHTML = ". . .";
-  document.getElementById("prevWords").innerHTML = "";
-  document.getElementById("randomImage").removeAttribute("src");
+  deleteTimers(wordTimer);
+  getText().addEventListener("keydown", startGame);
+  resetText();
+  resetRandomWords();
 }
 
 function downloadStory() {
-  const storyText = document.getElementById("storyText").value;
+  const storyText = getText().value;
   const now = new Date();
   const downloadLink = document.createElement("a");
   downloadLink.href = "data:x-application/text," + escape(storyText);
